24ab60f2e6390303fbc3b1944c4fa21f0578445f
SJiB <stefan.burckhardt@gmx.ch>
2024-05-06 19:53:08 +0200

    Add run-docker.sh
    Adapted from TWW to TDH and wastewater to distance_heating

---
+++ datamodel/scripts/run-docker.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -e
+
+# load env vars
+# https://stackoverflow.com/a/20909045/1548052
+export $(grep -v '^#' .env | xargs)
+
+BUILD=0
+DEMO_DATA=0
+TDH_PG_PORT=${TDH_PG_PORT:-5432}
+
+while getopts 'bdp:' opt; do
+  case "$opt" in
+    b)
+      echo "Rebuild docker image"
+      BUILD=1
+      ;;
+
+    d)
+      echo "Load demo data"
+      DEMO_DATA=1
+      ;;
+
+    p)
+      echo "Overriding PG port to ${OPTARG}"
+      TDH_PG_PORT=${OPTARG}
+      ;;
+
+
+    ?|h)
+      echo "Usage: $(basename $0) [-bd] [-p PG_PORT]"
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+if [[ $BUILD -eq 1 ]]; then
+  docker build --build-arg RUN_TEST=True -f datamodel/.docker/Dockerfile --tag teksi/distance_heating .
+fi
+
+docker rm -f teksi-distance_heating
+docker run -d -p ${TDH_PG_PORT}:5432 -v $(pwd):/src  --name teksi-distance_heating teksi/distance_heating -c log_statement=all
+docker exec teksi-distance_heating init_db.sh wait
+if [[ $DEMO_DATA -eq 1 ]]; then
+  docker exec teksi-distance_heating init_db.sh build -d
+fi

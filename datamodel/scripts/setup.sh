95b41e729c418f00efca54476f5866313b4971f5
SJiB <stefan.burckhardt@gmx.ch>
2024-05-21 14:37:35 +0200

    update setup.sh with 09_tdh_organisation_dictionaries.sql

--- datamodel/scripts/setup.sh
+++ datamodel/scripts/setup.sh
@@ -30,6 +30,7 @@ psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/changelogs/0001/03_tdh_
 
 # psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/changelogs/0001/05_data_model_extensions.sql
 
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/changelogs/0001/09_tdh_organisation_dictionaries.sql -v SRID=$SRID
 
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/changelogs/0001/09_tdh_dictionaries.sql -v SRID=$SRID
 

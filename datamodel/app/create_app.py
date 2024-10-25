6a91334b28364ee86013fa89636c76e26e249ee4
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2024-10-25 09:17:50 +0200

    Move functions where they belong
    Move functions where they belong

--- datamodel/app/create_app.py
+++ datamodel/app/create_app.py
@@ -58,6 +58,9 @@ def create_app(
 
     run_sql("CREATE SCHEMA tdh_app;", pg_service)
 
+    run_sql_file("functions/oid_functions.sql", pg_service, variables)
+    run_sql_file("functions/organisation_functions.sql", pg_service, variables)
+    
     # to do add symbology_function and geometry_funcions for TEKSI Distance heating
 
     #    run_sql_file("symbology_functions.sql", pg_service)

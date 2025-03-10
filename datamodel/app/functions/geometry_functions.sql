3dc384a9cf42143eabb1537fa2b0c7a648a91741
SJiB <stefan.burckhardt@gmx.ch>
2025-03-10 17:32:19 +0100

    replace to geometry3d_geometry (to fit the tdh translation)

--- datamodel/app/functions/geometry_functions.sql
+++ datamodel/app/functions/geometry_functions.sql
@@ -13,13 +13,13 @@ $BODY$
 BEGIN
   CASE
     WHEN TG_OP = 'INSERT' THEN
-      NEW.situation3d_geometry = ST_SetSRID( ST_MakePoint( ST_X(NEW.situation3d_geometry), ST_Y(NEW.situation3d_geometry), COALESCE(NEW.altitude1,'NaN') ), %1$s);
+      NEW.geometry3d_geometry = ST_SetSRID( ST_MakePoint( ST_X(NEW.geometry3d_geometry), ST_Y(NEW.geometry3d_geometry), COALESCE(NEW.altitude1,'NaN') ), %1$s);
     WHEN TG_OP = 'UPDATE' THEN
       IF NEW.altitude1 <> OLD.altitude1 OR (NEW.altitude1 IS NULL AND OLD.altitude1 IS NOT NULL) OR (NEW.altitude1 IS NOT NULL AND OLD.altitude1 IS NULL) THEN
-        NEW.situation3d_geometry = ST_SetSRID( ST_MakePoint( ST_X(NEW.situation3d_geometry), ST_Y(NEW.situation3d_geometry), COALESCE(NEW.altitude1,'NaN') ), %1$s);
+        NEW.geometry3d_geometry = ST_SetSRID( ST_MakePoint( ST_X(NEW.geometry3d_geometry), ST_Y(NEW.geometry3d_geometry), COALESCE(NEW.altitude1,'NaN') ), %1$s);
       ELSE
-        IF ST_Z(NEW.situation3d_geometry) <> ST_Z(OLD.situation3d_geometry) THEN
-          NEW.altitude1 = NULLIF(ST_Z(NEW.situation3d_geometry),'NaN');
+        IF ST_Z(NEW.geometry3d_geometry) <> ST_Z(OLD.geometry3d_geometry) THEN
+          NEW.altitude1 = NULLIF(ST_Z(NEW.geometry3d_geometry),'NaN');
         END IF;
       END IF;
   END CASE;
@@ -55,13 +55,13 @@ $BODY$
 BEGIN
   CASE
     WHEN TG_OP = 'INSERT' THEN
-      NEW.situation3d_geometry = ST_SetSRID( ST_MakePoint( ST_X(NEW.situation3d_geometry), ST_Y(NEW.situation3d_geometry), COALESCE(NEW.altitude,'NaN') ), %1$s);
+      NEW.geometry3d_geometry = ST_SetSRID( ST_MakePoint( ST_X(NEW.geometry3d_geometry), ST_Y(NEW.geometry3d_geometry), COALESCE(NEW.altitude,'NaN') ), %1$s);
     WHEN TG_OP = 'UPDATE' THEN
       IF NEW.altitude <> OLD.altitude OR (NEW.altitude IS NULL AND OLD.altitude IS NOT NULL) OR (NEW.altitude IS NOT NULL AND OLD.altitude IS NULL) THEN
-        NEW.situation3d_geometry = ST_SetSRID( ST_MakePoint( ST_X(NEW.situation3d_geometry), ST_Y(NEW.situation3d_geometry), COALESCE(NEW.altitude,'NaN') ), %1$s);
+        NEW.geometry3d_geometry = ST_SetSRID( ST_MakePoint( ST_X(NEW.geometry3d_geometry), ST_Y(NEW.geometry3d_geometry), COALESCE(NEW.altitude,'NaN') ), %1$s);
       ELSE
-        IF ST_Z(NEW.situation3d_geometry) <> ST_Z(OLD.situation3d_geometry) THEN
-          NEW.altitude = NULLIF(ST_Z(NEW.situation3d_geometry),'NaN');
+        IF ST_Z(NEW.geometry3d_geometry) <> ST_Z(OLD.geometry3d_geometry) THEN
+          NEW.altitude = NULLIF(ST_Z(NEW.geometry3d_geometry),'NaN');
         END IF;
       END IF;
   END CASE;

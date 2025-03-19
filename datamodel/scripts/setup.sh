02a116542e03c1f107daae1c7f54a02b6be5dc7d
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2025-03-19 11:26:37 +0100

    Redo #85
    Hopefully to avoid permission change from setup.sh

--- datamodel/scripts/setup.sh
+++ datamodel/scripts/setup.sh
@@ -34,6 +34,8 @@ psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/changelogs/0001/04_tdh_
 
 # psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/changelogs/0001/05_data_model_extensions.sql
 
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/changelogs/0001/05_add_vl_pipe_section_mg_material.sql
+
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/changelogs/0001/05_tdh_db_pipe_section_extra_attributes.sql
 
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/changelogs/0001/09_tdh_dictionaries.sql -v SRID=$SRID
